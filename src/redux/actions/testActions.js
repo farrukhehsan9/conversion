dbd0ef92c30785cc8cffa11f1350468ebec2d994
Farrukh Ehsan <farrukhehsan90@gmail.com>
2020-05-28 23:15:49 +0530

    ..

--- src/redux/actions/testActions.js
+++ src/redux/actions/testActions.js
@@ -5,6 +5,7 @@ const KELVIN_TO_FAHRENHEIT = "kelvin_fahrenheit";
 const FAHRENHEIT_TO_RANKINE = "fahrenheit_rankine";
 const RANKINE_TO_FAHRENHEIT = "rankine_fahrenheit";
 const CUPS_TO_LITRES = "cups_to_litres";
+const LITRES_TO_CUPS = "litres_to_cups";
 const GALLONS_TO_KELVIN = "gallons_kelvin";
 
 export const evaluate = (inputValue, measure, targetMeasure) => {
@@ -13,19 +14,22 @@ export const evaluate = (inputValue, measure, targetMeasure) => {
   switch (value) {
     case FAHRENHEIT_TO_KELVIN:
       answer = ((inputValue - 32) * 5) / 9 + 273.15;
-      return parseFloat(answer).toFixed(2);
+      return parseFloat(answer).toFixed(10);
     case KELVIN_TO_FAHRENHEIT:
       answer = ((inputValue - 273.15) * 9) / 5 + 32;
-      return parseFloat(answer).toFixed(2);
+      return parseFloat(answer).toFixed(10);
     case FAHRENHEIT_TO_RANKINE:
       answer = inputValue + 459.67;
-      return parseFloat(answer).toFixed(2);
+      return parseFloat(answer).toFixed(10);
     case RANKINE_TO_FAHRENHEIT:
       answer = inputValue - 459.67;
-      return parseFloat(answer).toFixed(2);
+      return parseFloat(answer).toFixed(10);
     case CUPS_TO_LITRES:
       answer = inputValue / 4.227;
-      return parseFloat(answer).toFixed(2);
+      return parseFloat(answer).toFixed(10);
+    case LITRES_TO_CUPS:
+      answer = inputValue * 4.227;
+      return parseFloat(answer).toFixed(10);
     default:
       answer = "invalid";
       return answer;
@@ -33,7 +37,7 @@ export const evaluate = (inputValue, measure, targetMeasure) => {
 };
 
 export const validate = (answer, studentResponse) => {
-  const responseInFloat = parseFloat(studentResponse).toFixed(2);
+  const responseInFloat = parseFloat(studentResponse).toFixed(10);
   if (answer !== responseInFloat) {
     return "incorrect";
   }
